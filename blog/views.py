4df8faebfae897bdd2acf8b7bb4f6f327f48b795
Giya001 <giyoshikmatillayev@gmail.com>
2025-06-24 21:57:57 +0500

    new

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Blog
+
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    a = 3
+    b = 4
+    c = a + b
+    context = {
+        'a': a,
+        'b': b,
+        'c': c,
+        'blogs': Blog.objects.all()
+    }
+    return render(request,'base.html',context=context)
